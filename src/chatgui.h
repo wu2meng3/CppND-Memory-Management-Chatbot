90adf36ed56fd40d05dfabb4fb2c409ab62da1f6
Meng Wu <wu1meng2@berkeley.edu>
2022-06-27 00:48:28 +0000

    Make ChatBotPanelDialog::_chatLogic a unique_ptr

--- src/chatgui.h
+++ src/chatgui.h
@@ -1,6 +1,6 @@
 #ifndef CHATGUI_H_
 #define CHATGUI_H_
-
+#include <memory>
 #include <wx/wx.h>
 
 class ChatLogic; // forward declaration
@@ -12,14 +12,7 @@ private:
     // control elements
     wxBoxSizer *_dialogSizer;
     wxBitmap _image;
-
-    //// STUDENT CODE
-    ////
-
-    ChatLogic *_chatLogic;
-
-    ////
-    //// EOF STUDENT CODE
+    std::unique_ptr<ChatLogic> _chatLogic;
 
 public:
     // constructor / destructor
@@ -27,7 +20,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic* GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
